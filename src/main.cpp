a401a03603fc08333c5f19d91a3cb37e55aa1f45
Unknown <fernandez.coralie2001@gmail.com>
2017-12-30 22:17:02 +0100

    2 lcd

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <Arduino.h>
 #include  <LiquidCrystal.h>
 
-const int rs = 11, en = 10, d0 = 9, d1 = 8, d2 = 7, d3 = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en,d0,d1,d2,d3,d4, d5, d6, d7);
-
+  const int rs = 12, e1 = 11, e2 = 10, d0 = 9, d1 = 8, d2 = 7, d3 = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+  LiquidCrystal lcd1(rs, e1, d0, d1, d2, d3, d4, d5, d6, d7);
+  LiquidCrystal lcd2(rs, e2, d0, d1, d2, d3, d4, d5, d6, d7);
 void setup() {
 	// put your setup code here, to run once:
-  lcd.begin(16, 2);
-  lcd.print("Hello my love <3");
+  lcd1.begin(16, 2);
+  lcd2.begin(16, 2);
+  lcd1.print("Hello my love!");
+  lcd2.print("I love you <3");
 }
 
 void loop() {
