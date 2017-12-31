f6aec58dfa3028da42e08757f9a4cdcb1a6e3210
Notraly <fernandez.coralie2001@gmail.com>
2017-12-31 12:01:47 +0100

    mise en forme

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,34 @@
 #include <Arduino.h>
 #include  <LiquidCrystal.h>
 
-  const int rs = 12, e1 = 11, e2 = 10, d0 = 9, d1 = 8, d2 = 7, d3 = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-  LiquidCrystal lcd1(rs, e1, d0, d1, d2, d3, d4, d5, d6, d7);
-  LiquidCrystal lcd2(rs, e2, d0, d1, d2, d3, d4, d5, d6, d7);
-  byte heart[8] = {
-    B00000,
-    B00000,
-    B01010,
-    B11111,
-    B01110,
-    B00100,
-    B00000,
-    B00000,
-  };
+const int rs = 12, e1 = 11, e2 = 10, d0 = 9, d1 = 8, d2 = 7, d3 = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd1(rs, e1, d0, d1, d2, d3, d4, d5, d6, d7);
+LiquidCrystal lcd2(rs, e2, d0, d1, d2, d3, d4, d5, d6, d7);
+byte heart[8] = {
+	B00000,
+	B00000,
+	B01010,
+	B11111,
+	B01110,
+	B00100,
+	B00000,
+	B00000,
+};
 void setup() {
 	// put your setup code here, to run once:
-  lcd2.createChar(0, heart);
-  lcd1.begin(16, 2);
-  lcd2.begin(16, 2);
-  lcd1.print("Hello my love!");
-  lcd2.print("Forever");
-  lcd1.setCursor(0, 1);
-  lcd2.setCursor(0, 1);
-  lcd1.print("I love you <3");
-  lcd2 .print("and ever");
-  lcd2.setCursor(9, 1);
-  lcd2.write(byte(0));
+	lcd2.createChar(0, heart);
+	lcd1.begin(16, 2);
+	lcd2.begin(16, 2);
+
+	lcd1.print("Hello my love!");
+	lcd1.setCursor(0, 1);
+	lcd1.print("I love you <3");
+
+	lcd2.print("Forever");
+	lcd2.setCursor(0, 1);
+	lcd2.print("and ever");
+	lcd2.setCursor(9, 1);
+	lcd2.write(byte(0));
 
 }
 
