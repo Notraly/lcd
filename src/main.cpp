856cbebec75107f6054312f0f57009173b941956
Notraly <fernandez.coralie2001@gmail.com>
2018-01-14 23:28:39 +0100

    déplacement joystick

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,26 @@ byte heart[8] = {
 	B00000,
 	B00000,
 };
+byte topBar[8] = {
+	B11111,
+	B00000,
+	B00000,
+	B00000,
+	B00000,
+	B00000,
+	B00000,
+	B00000,
+};
+byte bottomBar[8] = {
+	B00000,
+	B00000,
+	B00000,
+	B00000,
+	B00000,
+	B00000,
+	B00000,
+	B11111,
+};
 const float EFFET_FACTOR = 2;
 const byte PIN_POTENTIOMETRE = A0;
 const byte PIN_JOYSTICK1_X = A1;
@@ -29,8 +49,29 @@ long oldMillis;
 int deltaMillis;
 long counter = 0;
 float vitesse;
+// vX = vitesse en x et vY = vitesse en Y et x = position sur la ligne et y = position sur la colonne
+struct Raquettes {
+	float x, y, vX, vY;
+	byte pinX, pinY;
+	int row, column; // row = ligne de déplacement (x) et column = ligne de déplacement (y)
+	float scalVX,scalVY; // row = ligne de déplacement (x) et column = ligne de déplacement (y)
+};
+
+Raquettes joueur1 = {8,2,0,0,PIN_JOYSTICK1_X, PIN_JOYSTICK1_Y,0,0, 0.005, 0.0025};
+Raquettes joueur2 = {8,2,0,0,PIN_JOYSTICK2_X, PIN_JOYSTICK2_Y,3,15, 0.005, 0.0025};
+void writeLcds(int posX, int posY, byte data ){
+	if (posY < 2) {
+		lcd1.setCursor(posX, posY);
+		lcd1.write(data);
+	} else {
+		lcd2.setCursor(posX, posY - 2);
+		lcd2.write(data);
+	}
+
+}
 
 float convertValueJoystisk (int value){
+
 	if (value < 25) {
 		return -1;
 
@@ -48,12 +89,46 @@ float convertValueJoystisk (int value){
 	}
 }
 
-void dplJoystick1(){
-	int valeurX1 = analogRead(PIN_JOYSTICK1_X);
-	int valeurY1 = analogRead(PIN_JOYSTICK1_Y);
+void dplJoystick(Raquettes &joueur){
+	float valeurX = convertValueJoystisk(analogRead(joueur.pinX));
+	float valeurY = convertValueJoystisk(analogRead(joueur.pinY));
 
+	joueur.vX = valeurX * joueur.scalVX;
+	joueur.vY = valeurY * joueur.scalVY;
 
+	int oldPosX = joueur.x;
+	int newPosX;
+	int oldPosY = joueur.y;
+	int newPosY;
+
+	joueur.x += deltaMillis * joueur.vX;
+	joueur.y += deltaMillis * joueur.vY;
+
+	if (joueur.x < 0) {
+		joueur.x = 0;
+	} else if(joueur.x >= 16) {
+		joueur.x = 15.99;
+	}
+
+	if (joueur.y < 0) {
+		joueur.y = 0;
+	} else if (joueur.y >= 4) {
+		joueur.y = 3.99;
+	}
+
+	newPosY = joueur.y;
+	newPosX = joueur.x;
+
+	if (newPosX != oldPosX  ) {
+		writeLcds(oldPosX, joueur.row, byte( 0x20 )); // 0x = ce qu'il y a derrière est en hexadécimale
+		writeLcds(newPosX, joueur.row, byte(1));
+	}
+	if (newPosY != oldPosY) {
+		writeLcds(joueur.column, oldPosY, byte( 0x20 )); // 0x = ce qu'il y a derrière est en hexadécimale
+		writeLcds(joueur.column, newPosY, byte(0x7C));
+	}
 }
+
 void testJ1() {
 	float valeurX1 = -convertValueJoystisk(analogRead(PIN_JOYSTICK1_X));
 	float valeurY1 = convertValueJoystisk(analogRead(PIN_JOYSTICK1_Y));
@@ -86,6 +161,7 @@ void testJ2() {
 	lcd2.print("   ");
 
 }
+
 float randFactor(float maxF){
 	int r = random(-64,64);
 	if(r<0) {
@@ -100,15 +176,14 @@ void updateV(){
 	vitesse /= 100000;
 }
 
-void writeLcds(int posX, int posY, byte data ){
-	if (posY < 2) {
-		lcd1.setCursor(posX, posY);
-		lcd1.write(data);
-	} else {
-		lcd2.setCursor(posX, posY - 2);
-		lcd2.write(data);
-	}
-
+void refresh(){
+	lcd1.clear();
+	lcd2.clear();
+	writeLcds(joueur1.x, joueur1.row, byte(1));
+	writeLcds(joueur1.column, joueur1.y, byte(0x7C));
+	writeLcds(joueur2.x, joueur2.row, byte(1));
+	writeLcds(joueur2.column, joueur2.y, byte(0x7C));
+	writeLcds(posX, posY, byte(0));
 }
 
 //deplacementBalle = methode pour le déplacement de la balle
@@ -157,32 +232,61 @@ void deplacementBalle(){
 	}
 }
 
+void fps (){
+
+	counter++;
+
+	if((counter & 127) == 127) {
+		long fps = counter * 1000 / millis();
+		lcd1.home();
+		lcd1.print(fps);
+		lcd1.print(" fps ");
+	}
+}
+
 void setup() {
 	lcd1.createChar(0, heart);
 	lcd2.createChar(0, heart);
+	lcd1.createChar(1, topBar);
+	lcd2.createChar(1, bottomBar);
 	lcd1.begin(16, 2);
 	lcd2.begin(16, 2);
 	oldMillis = millis();
 	Serial.begin(9600);
-}
+	refresh();
 
+}
+void debugBalle(){
+	Serial.print("debugBalle 	");
+	Serial.print(posX);
+	Serial.print(" 	");
+	Serial.print(posY);
+	Serial.print(" 	");
+	Serial.print(vitesse);
+	Serial.print(" 	");
+	Serial.print(vitesseX);
+	Serial.print(" 	");
+	Serial.print(vitesseY);
+	Serial.print(" 	");
+	Serial.println(analogRead(A0));
+}
+void debug(){
+	debugBalle();
+	delay(200);
+}
 void loop() {
 	long newMillis = millis();
 	deltaMillis = newMillis - oldMillis;
 	// START LOOP
 	updateV();
-	// deplacementBalle();
-	testJ1();
-	testJ2();
-	delay(250);
+	deplacementBalle();
+	dplJoystick(joueur1);
+	dplJoystick(joueur2);
+	debug();
 	// END LOOP
 	oldMillis = newMillis;
-	counter++;
 
-	if((counter & 127) == 127) {
-		long fps = counter * 1000 / newMillis;
-		lcd1.home();
-		lcd1.print(fps);
-		lcd1.print(" fps ");
-	}
+	//fps();
+
+
 }
